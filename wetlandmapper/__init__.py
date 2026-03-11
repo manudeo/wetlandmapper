54f8b818087dc54cef9051c572cc9644fa6f38bb
Manudeo Singh, PhD <54727984+manudeo@users.noreply.github.com>
2026-03-11 22:36:59 +0000

    Update __init__.py

--- wetlandmapper/__init__.py
+++ wetlandmapper/__init__.py
@@ -1,6 +1,9 @@
 """
 WetlandMapper
 =============
+# Copyright (c) 2026, Manudeo Singh          #
+# Author: Manudeo Singh, March 2026          #
+
 Automatic wetland detection, dynamics classification, and Wetland Cover Type (WCT)
 characterisation from multispectral satellite time-series data.
 
@@ -18,16 +21,29 @@ Quick start
 See the package documentation and demo notebook for full usage examples.
 """
 
-from importlib.metadata import version, PackageNotFoundError
+from importlib.metadata import PackageNotFoundError, version
 
 try:
     __version__ = version("wetlandmapper")
 except PackageNotFoundError:
     __version__ = "unknown"
 
-from .indices import compute_mndwi, compute_ndvi, compute_ndti, compute_indices
-from .dynamics import classify_dynamics, DYNAMICS_CLASSES, DYNAMICS_COLORS, compute_wet_frequency, aggregate_time
-from .wct import classify_wct, classify_wct_ema, WCT_CLASSES, WCT_COLORS, WCT_EMA_QUARTILE_BOUNDARIES, build_ema_lookup_table
+from .dynamics import (
+    DYNAMICS_CLASSES,
+    DYNAMICS_COLORS,
+    aggregate_time,
+    classify_dynamics,
+    compute_wet_frequency,
+)
+from .indices import compute_indices, compute_mndwi, compute_ndti, compute_ndvi
+from .wct import (
+    WCT_CLASSES,
+    WCT_COLORS,
+    WCT_EMA_QUARTILE_BOUNDARIES,
+    build_ema_lookup_table,
+    classify_wct,
+    classify_wct_ema,
+)
 
 __all__ = [
     # Index computation
